de872cdfd65b9a7006b1095e735dadd2a1b0fe46
fco-romario <fco.romario.alves@gmail.com>
2025-09-27 14:38:57 -0300

    Adicionando Projeto Base - Spring Boot

---
+++ loja-ceda-artes/src/main/java/br/com/fco_romario/loja_ceda_artes/LojaCedaArtesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fco_romario.loja_ceda_artes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaCedaArtesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaCedaArtesApplication.class, args);
+	}
+
+}
---
+++ loja-ceda-artes/src/test/java/br/com/fco_romario/loja_ceda_artes/LojaCedaArtesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fco_romario.loja_ceda_artes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaCedaArtesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
