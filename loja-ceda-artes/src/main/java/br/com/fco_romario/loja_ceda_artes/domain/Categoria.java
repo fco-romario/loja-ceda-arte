6f94ba5cceda4d9623ace051f37c8eeded7c9674
fco-romario <fco.romario.alves@gmail.com>
2025-09-27 15:26:00 -0300

    Criando a primeira Entidade Categoria

---
+++ loja-ceda-artes/src/main/java/br/com/fco_romario/loja_ceda_artes/domain/Categoria.java
@@ -0,0 +1,54 @@
+package br.com.fco_romario.loja_ceda_artes.domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    public Categoria() {}
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(getId(), categoria.getId()) && Objects.equals(getNome(), categoria.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome());
+    }
+}
