ae2a0f4994f4df6cf54d765d593a61ada8d7717b
fco-romario <fco.romario.alves@gmail.com>
2025-10-04 16:38:23 -0300

    Criando CRUD Controller e Service para Cliente

--- loja-ceda-artes/src/main/java/br/com/fco_romario/loja_ceda_artes/controllers/ClienteController.java
+++ loja-ceda-artes/src/main/java/br/com/fco_romario/loja_ceda_artes/controllers/ClienteController.java
@@ -3,11 +3,12 @@ package br.com.fco_romario.loja_ceda_artes.controllers;
 import br.com.fco_romario.loja_ceda_artes.domain.Cliente;
 import br.com.fco_romario.loja_ceda_artes.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/clientes")
@@ -16,10 +17,35 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping(value = "/buscarPorId/{id}")
-    public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Cliente>> buscarTodos() {
+        List<Cliente> list = clienteService.buscarTodos();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Integer id) {
         Cliente obj = clienteService.buscarPorId(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cliente> criar(@RequestBody Cliente cliente) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.criar(cliente));
+    }
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cliente> atualizar(@RequestBody Cliente cliente) {
+        return ResponseEntity.ok().body(clienteService.atualizar(cliente));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Integer id) {
+        clienteService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- loja-ceda-artes/src/main/java/br/com/fco_romario/loja_ceda_artes/services/ClienteService.java
+++ loja-ceda-artes/src/main/java/br/com/fco_romario/loja_ceda_artes/services/ClienteService.java
@@ -6,6 +6,7 @@ import br.com.fco_romario.loja_ceda_artes.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,30 @@ public class ClienteService {
         Optional<Cliente> obj = clienteRepository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(
                 "Objecto não encontrado id: " + id + ", tipo: " + Cliente.class.getSimpleName()));
+    }
+
+    public List<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente criar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente atualizar(Cliente cliente) {
+        Cliente obj = buscarPorId(cliente.getId());
+
+        obj.setNome(cliente.getNome());
+        obj.setEmail(cliente.getEmail());
+        obj.setCpfOuCnpj(cliente.getCpfOuCnpj());
+        obj.setTipo(cliente.getTipo());
+
+        return clienteRepository.save(cliente);
+    }
+
+    public void deletar(Integer id) {
+        Cliente obj = buscarPorId(id);
+
+        clienteRepository.delete(obj);
     }
 }
